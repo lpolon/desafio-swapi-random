b729d561af6e6f05f451d3330efa3e35d6193841
lpolon <lpolon@gmail.com>
2020-03-03 22:01:12 -0300

    app.jsx is stateful, fetches all planets in its first render
    in its first render, app.jsx puts in its state the array on planets and one random planet.
    it has a function to pick one random planet and update it
    it handles first loading and errors from Api

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
+import 'bulma-pageloader';
 import './App.css';
 
-import { Swapi } from '../../Util/Swapi';
+import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
+import { faRebel } from '@fortawesome/free-brands-svg-icons';
+
+import Swapi from '../../Util/Swapi';
+import { getOneRandomArrayElement } from '../../Util/helper';
+const planetsResource = new Swapi('planets');
+
 export default function App() {
-  return <div className="App"></div>;
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [planets, setPlanets] = useState([]);
+  const [randomPlanet, setRandomPlanet] = useState({});
+
+  const fetchAllPlanets = async () => {
+    setIsLoading(true);
+    const response = await planetsResource.getAllResources();
+    if (typeof response === 'string') {
+      setError(response);
+      setIsLoading(false);
+    } else {
+      const planet = getOneRandomArrayElement(response);
+      setPlanets(response);
+      setRandomPlanet(planet);
+      setIsLoading(false);
+    }
+  };
+
+  const handleClick = () => {
+    const planet = getOneRandomArrayElement(planets);
+    setRandomPlanet(planet);
+  };
+
+  useEffect(() => {
+    fetchAllPlanets();
+    // eslint-disable-next-line
+  }, []);
+
+  console.log('oi!', randomPlanet);
+
+  return isLoading ? (
+    <div className="pageloader is-active has-background-dark">
+      <div className="loader-text-flex">
+        <div className="is-size-3 has-text-light">
+          Restoring freedom to the galaxy...
+        </div>
+        <div className="icon-container">
+          <FA icon={faRebel} size="4x" color="hsl(0, 0%, 96%)" />
+        </div>
+      </div>
+    </div>
+  ) : error !== null ? (
+    <h1
+      className="content title is-size-1 is-white has-text-danger"
+      style={{ textAlign: 'center' }}
+    >
+      {error}
+    </h1>
+  ) : (
+    <div className="App">
+      <h1>oi!</h1>
+    </div>
+  );
 }
---
+++ src/Util/helper.js
@@ -0,0 +1,3 @@
+export const getOneRandomArrayElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
