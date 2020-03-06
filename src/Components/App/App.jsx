f45503c725fc4c40c79634f121472d6b656ed631
lpolon <lpolon@gmail.com>
2020-03-06 01:34:46 -0300

    wip

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bulma/css/bulma.css';
 import 'bulma-pageloader';
 import './App.css';
-
-import { useTransition, animated } from 'react-spring';
+import { useTransition, animated, config } from 'react-spring';
 
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
 import { faRebel } from '@fortawesome/free-brands-svg-icons';
@@ -16,24 +15,12 @@ import GetRandomPlanet from '../GetRandomPlanetButton';
 const planetsResource = new Swapi('planets');
 
 export default function App() {
+  console.log('oi, render!!!!');
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [planets, setPlanets] = useState([]);
   const [randomPlanet, setRandomPlanet] = useState({});
-
-  const [number, set] = useState(1);
-  const transitions = useTransition(number, null, {
-    from: {
-      opacity: 0,
-      transform: 'translate3d(100%,0,0)',
-    },
-    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: {
-      opacity: 0,
-      transform: 'translate3d(-100%,0,0)',
-      position: 'absolute',
-    },
-  });
+  const [index, setIndex] = useState(0);
 
   const fetchAllPlanets = async () => {
     setIsLoading(true);
@@ -51,15 +38,42 @@ export default function App() {
 
   const handleInput = () => {
     const planet = getOneRandomArrayElement(planets);
-    let newNumber = Number(number);
-    set((newNumber += 1));
-    setRandomPlanet(planet);
+    // setRandomPlanet(planet);
+    setIndex((state) => (state + 1) % 2)
   };
 
   useEffect(() => {
+    console.log('eu só deveria rodar uma vez!');
     fetchAllPlanets();
     // eslint-disable-next-line
   }, []);
+
+  // animation:
+  const prevRandomPlanet = usePrevious(randomPlanet);
+  const planetCardsArr = [
+    ({ style }) => (<animated.div style={{ ...style }}><PlanetCard {...randomPlanet} /></animated.div>),
+    ({ style }) => (<animated.div style={{ ...style }}><PlanetCard {...prevRandomPlanet} /></animated.div>),
+  ];
+  console.log('random planet!', randomPlanet);
+  console.log('random planet anterior!!', prevRandomPlanet);
+
+  const transitions = useTransition(
+    index,
+    planet => planet,
+    {
+      from: {
+        opacity: 0,
+        transform: 'translate3d(100%,0,0)',
+      },
+      enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+      leave: {
+        opacity: 0,
+        transform: 'translate3d(-100%,0,0)',
+        position: 'absolute',
+      },
+      config: config.default,
+    }
+  );
 
   return isLoading ? (
     <PageLoader />
@@ -72,13 +86,10 @@ export default function App() {
     </h1>
   ) : (
     <div className="App has-background-light">
-      <div>
-        {transitions.map(({ item, key, props }) => (
-          <animated.div key={key} style={props}>
-            <PlanetCard {...randomPlanet} />
-          </animated.div>
-        ))}
-      </div>
+      {transitions.map(({ item, props, key }) => {
+        const Planet = planetCardsArr[item];
+        return <Planet key={key} style={props} />;
+      })}
       <div>
         <GetRandomPlanet onInput={handleInput} />
       </div>
@@ -99,4 +110,12 @@ function PageLoader() {
       </div>
     </div>
   );
+}
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
 }
